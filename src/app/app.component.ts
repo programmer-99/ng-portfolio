b01a029449b2f233b51f94278ae927fb5ce84718
Muhammad Usama <m99usama@gmail.com>
2025-04-24 21:16:25 +0500

    views feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
+import { initializeApp } from 'firebase/app'; 
+import { getFirestore, doc, updateDoc, increment, getDoc } from 'firebase/firestore'; 
+import { firebaseConfig } from './firebase-config';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  private app = initializeApp(firebaseConfig); 
+  private firestore = getFirestore(this.app); 
+  private analytics = getAnalytics(this.app); 
+  viewCount: number = 0;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.incrementViewCount();
+    this.fetchViewCount();
+    logEvent(this.analytics, 'view_portfolio'); 
+  }
+
+  incrementViewCount() {
+    const viewsRef = doc(this.firestore, 'views/portfolio');
+    updateDoc(viewsRef, {
+      count: increment(1) 
+    }).then(() => {
+      console.log('View count updated');
+    }).catch((error) => {
+      console.error('Error updating view count: ', error);
+    });
+  }
+
+  fetchViewCount() {
+    const viewsRef = doc(this.firestore, 'views/portfolio');
+    getDoc(viewsRef).then((docSnapshot) => {
+      if (docSnapshot.exists()) {
+        this.viewCount = docSnapshot.data()?.['count']; 
+      }
+    }).catch((error) => {
+      console.error('Error fetching view count: ', error);
+    });
+  }
 }
---
+++ src/app/firebase-config.ts
@@ -0,0 +1,9 @@
+export  const firebaseConfig = {
+    apiKey: "AIzaSyAlOgFV5v3W5GWnMI41OAg-LeQS2xZSwf8",
+    authDomain: "portfolio-a476e.firebaseapp.com",
+    projectId: "portfolio-a476e",
+    storageBucket: "portfolio-a476e.firebasestorage.app",
+    messagingSenderId: "298694817933",
+    appId: "1:298694817933:web:fd404aeb5b6e0246dc4ac4",
+    measurementId: "G-NPJLW1T3X1"
+  };
